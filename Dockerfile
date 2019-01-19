f89336231d450b7c950d6f9c19893ee61087de13
Noah Watkins <noahwatkins@gmail.com>
2019-01-19 10:02:16 -0800

    wip
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 from alpine:latest
 LABEL "com.github.actions.name"="Hugo Build"
 LABEL "com.github.actions.description"="Hugo Build Description"
-RUN apk add --no-cache bash
+RUN apk add --no-cache bash ca-certificates hugo
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo $PWD
+SITE_SRC=${PWD}
+MAIN_URL=https://nwat.xyz
+WIP_URL=https://wip.nwat.xyz
+
+DRAFT_DIR=$(mktemp -d)
+trap "rm -rf ${DRAFT_DIR}" EXIT
+
+branches=$(git ls-remote --heads ${SITE_SRC} | sed 's?.*refs/heads/??')
+for branch in ${branches}; do
+  url=${WIP_URL}/${branch}
+  sha=$(git ls-remote ${SITE_DIR} refs/heads/${branch} | cut -f 1)
+
+  if curl --output /dev/null --silent --head --fail "${url}/${sha}.cookie"; then
+    exclude_dirs+=(${branch})
+    echo "Branch ${branch} is already up to date... skipping"
+  else
+    echo "will build"
+    #build ${WIP_URL}/${branch} ${branch} ${DRAFT_DIR}/${branch} -D -F
+  fi
+done
